a08313907171c57d000a70ea244dc212dc941d5d
Kyo Chou <develop.kyo@gmail.com>
2024-05-06 19:15:50 +0800

    error ext

---
+++ entry_ext.go
@@ -0,0 +1,71 @@
+package logrus
+
+func (entry *Entry) ELog(level Level, err error, args ...interface{}) {
+	if err != nil {
+		args = append(args, err)
+		entry.Log(level, args...)
+	}
+}
+
+func (entry *Entry) ELogln(level Level, err error, args ...interface{}) {
+	if err != nil {
+		args = append(args, err)
+		entry.Logln(level, args...)
+	}
+}
+
+func (entry *Entry) ELogf(level Level, err error, format string, args ...interface{}) {
+	if err != nil {
+		entry.Logf(level, format, args...)
+	}
+}
+
+func (entry *Entry) EError(err error, args ...interface{}) {
+	entry.ELog(ErrorLevel, err, args...)
+}
+func (entry *Entry) EErrorln(err error, args ...interface{}) {
+	entry.ELogln(ErrorLevel, err, args...)
+}
+func (entry *Entry) EErrorf(err error, format string, args ...interface{}) {
+	entry.ELogf(ErrorLevel, err, format, args...)
+}
+
+func (entry *Entry) EWarn(err error, args ...interface{}) {
+	entry.ELog(WarnLevel, err, args...)
+}
+func (entry *Entry) EWarnln(err error, args ...interface{}) {
+	entry.ELogln(WarnLevel, err, args...)
+}
+func (entry *Entry) EWarnf(err error, format string, args ...interface{}) {
+	entry.ELogf(WarnLevel, err, format, args...)
+}
+
+func (entry *Entry) EInfo(err error, args ...interface{}) {
+	entry.ELog(InfoLevel, err, args...)
+}
+func (entry *Entry) EInfoln(err error, args ...interface{}) {
+	entry.ELogln(InfoLevel, err, args...)
+}
+func (entry *Entry) EInfof(err error, format string, args ...interface{}) {
+	entry.ELogf(InfoLevel, err, format, args...)
+}
+
+func (entry *Entry) EDebug(err error, args ...interface{}) {
+	entry.ELog(DebugLevel, err, args...)
+}
+func (entry *Entry) EDebugln(err error, args ...interface{}) {
+	entry.ELogln(DebugLevel, err, args...)
+}
+func (entry *Entry) EDebugf(err error, format string, args ...interface{}) {
+	entry.ELogf(DebugLevel, err, format, args...)
+}
+
+func (entry *Entry) ETrace(err error, args ...interface{}) {
+	entry.ELog(TraceLevel, err, args...)
+}
+func (entry *Entry) ETraceln(err error, args ...interface{}) {
+	entry.ELogln(TraceLevel, err, args...)
+}
+func (entry *Entry) ETracef(err error, format string, args ...interface{}) {
+	entry.ELogf(TraceLevel, err, format, args...)
+}
